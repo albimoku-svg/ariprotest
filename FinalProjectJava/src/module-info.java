f2902314862901f080a34b4e413bb34ac1c97a77
albimoku-svg <albimoku@gmail.com>
2026-01-18 14:13:46 +0100

    Add files via upload

---
+++ FinalProjectJava/src/module-info.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module FinalProjectJava {
+    requires javafx.controls;
+    requires javafx.fxml;
+	requires javafx.base;
+
+    opens First to javafx.fxml;
+    opens CostumerManagment to javafx.fxml; 
+    opens inventory to javafx.fxml;
+
+    exports First;
+    exports CostumerManagment;
+    exports inventory;
+}
